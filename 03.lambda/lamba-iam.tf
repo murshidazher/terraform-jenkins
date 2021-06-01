0b3358596300413d8fb7801f901ad0e1573cdd3d
murshidazher <murshidazher@yahoo.com>
2021-06-02 01:42:53 +0530

    :recycle: refactor: extract policy

--- 03.lambda/lamba-iam.tf
+++ 03.lambda/lamba-iam.tf
@@ -2,20 +2,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = aws_iam_role.test_role.id
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+  policy = file("./iam/lambda-policy.json")
 }
 
 resource "aws_iam_role" "test_role" {
