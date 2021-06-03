e45e28ecb768ea4bf491218a66c91d865d19d97d
murshidazher <murshidazher@yahoo.com>
2021-06-03 14:22:26 +0530

    :sparkles: feat: add lambda role

--- 03.lambda/lamba-iam.tf
+++ 03.lambda/lamba-iam.tf
@@ -1,24 +1,12 @@
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
-  role = aws_iam_role.test_role.id
+  role = aws_iam_role.lambda_role.id
 
   policy = file("./iam/lambda-policy.json")
 }
 
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+  assume_role_policy = file("./iam/lambda-role.json")
 }
